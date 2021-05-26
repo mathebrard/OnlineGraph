a36aae23dd3a6eb8effaac3758e7fe466e7cf3c3
HOGIE Luc <luc.hogie@inria.fr>
2021-05-26 11:14:25 +0200

    initial commit

---
+++ src/main/java/Graph.java
@@ -0,0 +1,38 @@
+import toools.io.file.Directory;
+import toools.io.file.RegularFile;
+
+public class Graph extends AbstractGraph {
+
+	final Directory d;
+
+	public static final Directory baseDirectory = new Directory("$HOME/graph-server");
+
+	static {
+		baseDirectory.ensureExists();
+	}
+
+	public Graph(Object id) {
+		d = new Directory(baseDirectory, id.toString());
+	}
+
+	void addVertex(String u) {
+		RegularFile vf = new RegularFile(d, u);
+		vf.create();
+	}
+
+	void removeVertex(String u) {
+		RegularFile vf = new RegularFile(d, u);
+		vf.delete();
+	}
+
+	void addEdge(String from, String to) {
+		RegularFile f = new RegularFile(d, from + " --- " + to);
+		f.create();
+	}
+
+	void removeEdge(String from, String to) {
+		RegularFile f = new RegularFile(d, from + " --- " + to);
+		f.delete();
+	}
+
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import idawi.Component;
+import idawi.service.ServiceManager;
+import idawi.service.rest.RESTService;
+
+public class Main {
+	public static void main(String[] args) {
+		Component c = new Component();
+		c.lookupService(ServiceManager.class).ensureStarted(RESTService.class);
+		
+	}
+}
