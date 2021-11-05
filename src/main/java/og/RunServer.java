4fe3a112a775b4f23299491511cebb46083db2cd
HOGIE Luc <luc.hogie@inria.fr>
2021-11-05 16:39:03 +0100

    ko

--- src/main/java/og/RunServer.java
+++ src/main/java/og/RunServer.java
@@ -3,32 +3,27 @@ package og;
 import java.io.IOException;
 
 import idawi.Component;
-import idawi.ComponentDescriptor;
 import idawi.service.ServiceManager;
 import idawi.service.rest.RESTService;
 import toools.thread.Threads;
 
 public class RunServer {
 	public static void main(String[] args) throws IOException {
-		
-		
 		var port = args.length == 0 ? 8082 : Integer.parseInt(args[0]);
-		var descriptor = new ComponentDescriptor();
-		descriptor.friendlyName = "og";
-		Component c = new Component(descriptor);
+		Component c = new Component();
+		c.friendlyName = "og";
 		c.lookupService(ServiceManager.class).ensureStarted(GraphService.class);
 		var gs = c.lookupService(GraphService.class);
 		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
 			gs.close();
 		}));
 
-		
 		c.lookupService(ServiceManager.class).ensureStarted(RESTService.class);
 
-		
 		var rest = c.lookupService(RESTService.class);
 		rest.startHTTPServer(port);
 		System.out.println("URL: http://localhost:" + port + "/api/" + c.friendlyName);
+		System.out.println("Website URL: http://localhost:" + port + "/web/og/display/ls.html");
 		Threads.sleepForever();
 	}
 }
