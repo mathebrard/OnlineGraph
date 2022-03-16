86d4ee3fe94107a2559db7f750dd6cf1e03f45f1
HOGIE Luc <luc.hogie@inria.fr>
2022-03-16 10:09:59 +0100

    fix

--- src/main/java/og/RunGraphServer.java
+++ src/main/java/og/RunGraphServer.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 import idawi.Component;
 import idawi.service.ServiceManager;
-import idawi.service.rest.RESTService;
+import idawi.service.rest.WebServer;
 import toools.thread.Threads;
 
 public class RunGraphServer {
@@ -18,9 +18,9 @@ public class RunGraphServer {
 			gs.close();
 		}));
 
-		c.lookup(ServiceManager.class).ensureStarted(RESTService.class);
+		c.lookup(ServiceManager.class).ensureStarted(WebServer.class);
 
-		var rest = c.lookup(RESTService.class);
+		var rest = c.lookup(WebServer.class);
 		rest.startHTTPServer(port);
 		System.out.println("URL: http://localhost:" + port + "/api/" + c.name);
 		System.out.println("Website URL: http://localhost:" + port + "/web/og/display/ls.html");
