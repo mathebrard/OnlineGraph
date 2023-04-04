cd8cc88d1dae725cfef005586786ebfd15d3d8e3
lhogie <luc.hogie@inria.fr>
2023-04-04 16:28:14 +0200

    clean

--- release2i3s.sh
+++
@@ -1,9 +0,0 @@
-set -e
-rm -rf target/*.jar target/dependency/*jar onlinegraph-jars/
-mvn package
-mvn dependency:copy-dependencies
-mkdir onlinegraph-jars
-cp target/*.jar target/dependency/*jar onlinegraph-jars/
-tar czvf onlinegraph-jars.tgz onlinegraph-jars
-rm -rf onlinegraph-jars dependency-reduced-pom.xml
-scp onlinegraph-jars.tgz hogie@bastion.i3s.unice.fr:public_html/software/onlineGraph/
