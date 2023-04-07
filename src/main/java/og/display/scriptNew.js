78322bd953799a7178bfa06746cc4548a3c4cdbd
mathebrard <mathebrard47@gmail.com>
2023-04-07 14:31:20 +0200

    adding position fixed for initgraph

--- src/main/java/og/display/scriptNew.js
+++ src/main/java/og/display/scriptNew.js
@@ -8,85 +8,69 @@ source.addEventListener("message", (event) => {
   var lines = data.split(/\r?\n/);
   var headerraw = lines.shift();
   var temp = lines.join("");
-  var jsonToProcess = JSON.parse(temp);
+  if(temp !== "EOT") {
+      var jsonToProcess = JSON.parse(temp);
+      var jsonFormatted = parseJaseto(jsonToProcess);
+    
+      initGraph(jsonFormatted);
+  }
 
-  var jsonFormatted = parseJaseto(jsonToProcess);
-  initGraph(jsonFormatted);
-  
-  console.log("json formated = ", jsonFormatted);
 });
 
+
+/**
+ * Print the graph updated with  a JSON given in parameter
+ * @param {*} data the JSON formated in order to print the graph correctly
+ * @returns 
+ */
 function initGraph(data) {
   let init = [], arcsBis = [], listOfVerticesIds = [];
-  data["vertices"].forEach((vertex) => {
+  data.vertices.forEach((vertex) => {
     listOfVerticesIds.push(vertex.id)
+    console.log("vertex : ", vertex)
     init.push({
+        fx: vertex.x*150,
+        fy: vertex.y*150,
+        fz: vertex.z*150,
       id: vertex.id,
       vx: 0,
       vy: 0,
       vz: 0,
+      color: vertex.properties.color,
+      description: vertex.properties.label,
+      width: vertex.properties.width
     });
   });
 
-  data["arcs"].forEach((arc) => {
+  data.arcs.forEach((arc) => {
+    // Create arcs only if vertices to link exist
     if(listOfVerticesIds.includes(arc.from) && listOfVerticesIds.includes(arc.to)) {
         arcsBis.push({
-          id: arc["id"],
+          id: arc.id,
           source: arc.from,
           target: arc.to,
+          shape: arc.properties.shape
         });
     }
   });
-  console.log("arcbis : ", arcsBis, " list ids : ", listOfVerticesIds, " init : ", init)
 
   let Graph = ForceGraph3D()(document.getElementById("3d-graph"))
     // .linkColor(() => "rgba(255, 255, 255, 1)")
     .linkWidth(1)
-    // .linkColor((link) => {
-    //   const { source, target } = link;
-    //   const dx = Math.abs(source.x - target.x);
-    //   const dy = Math.abs(source.y - target.y);
-    //   const dz = Math.abs(source.z - target.z);
-    //   const dist = dx + dy + dz;
-    //   const opacity = 1 - dist / D;
-    //   return `rgba(255, 255, 255, ${opacity})`;
-    // })
+    .linkColor((link) => {
+      //   const { source, target } = link;
+      //   const dx = Math.abs(source.x - target.x);
+      //   const dy = Math.abs(source.y - target.y);
+      //   const dz = Math.abs(source.z - target.z);
+      //   const dist = dx + dy + dz;
+      //   const opacity = 1 - dist / D;
+      return link.color;
+    })
+    .nodeLabel("description")
     .linkOpacity(1.0);
 
   Graph.cooldownTime(Infinity)
-//     .d3AlphaDecay(0)
-//     .d3VelocityDecay(0)
-
-//     // Deactivate existing forces
-//     .d3Force("center", null)
-//     .d3Force("charge", null)
-//     .d3Force("link", null)
-
-//     // Add collision and bounding box forces
-//     .d3Force("collide", d3.forceCollide(Graph.nodeRelSize()))
-//     .d3Force("box", () => {
-//       const { nodes, links } = Graph.graphData();
-//       const CUBE_HALF_SIDE = Graph.nodeRelSize() * 30 * 0.5;
-
-//       nodes.forEach((node) => {
-//         const x = node.x || 0,
-//           y = node.y || 0,
-//           z = node.z || 0;
-
-//         // bounce on box walls
-//         if (Math.abs(x) > CUBE_HALF_SIDE) {
-//           node.vx *= -1;
-//         }
-//         if (Math.abs(y) > CUBE_HALF_SIDE) {
-//           node.vy *= -1;
-//         }
-//         if (Math.abs(z) > CUBE_HALF_SIDE) {
-//           node.vz *= -1;
-//         }
-//       });
-//     })
-
-    // Add nodes
+    // Add nodes and links
     .graphData({ nodes: init, links: arcsBis });
 
   Graph.onEngineTick(() => {
@@ -100,7 +84,7 @@ function initGraph(data) {
 
 
 /**
- * 
+ * parse the json in order to make it fit for initGraph method
  * @param {*} jsonObject to change in order to make it fit the function initGraph that will print the graph correctly
  * @returns 
  */
@@ -112,57 +96,58 @@ function parseJaseto(jsonObject){
 
     // Iterate over objects of jaseto entity 
     for (let element of Object.entries(jsonObject)) {
-
-        // If one of the objects is of type arc, we retrieve arcs contained in it
-        if(element[0] === 'arcs'){
-            for (let arc of element[1].elements) {
-                //retrieve every properties of every arc that has properties different from null
-                if (arc.properties !== null && arc.properties !== undefined) {
-                    let temp = {
-                        id: arc.id,
-                        from: arc.from,
-                        to: arc.to,
-                        properties: arc.properties
-                    };
-                    arcs.push(temp);
-                }
-            }
-        }
-
-        if (element[0] === "edges") {
-          for (let edge of element[1].elements) {
-            //retrieve every properties of every arc that has properties different from null
-            if (edge.properties !== null && edge.properties !== undefined) {
-              let temp = {
-                id: edge.id,
-                ends: edge.ends,
-                properties: edge.properties,
-              };
-              edges.push(temp);
-            }
+      // If one of the objects is of type arc, we retrieve arcs contained in it
+      if (element[0] === "arcs") {
+        for (let arc of element[1].elements) {
+          //retrieve every properties of every arc that has properties different from null
+          if (arc.properties !== null && arc.properties !== undefined) {
+            let temp = {
+              id: arc.id,
+              from: arc.from,
+              to: arc.to,
+              properties: arc.properties,
+            };
+            arcs.push(temp);
           }
         }
+      }
 
-        if (element[0] === "vertices") {
-          for (let vertex of element[1].elements) {
-            //retrieve every properties of every arc that has properties different from null
-            if (vertex.properties !== null && vertex.properties !== undefined) {
-                let locations = vertex.properties.location.split(",");
-                let x = locations[0];
-                let y = locations[1];
-                let z = locations[2];
-                
-              let temp = {
-                id: vertex.id,
-                x: x,
-                y: y,
-                z: z,
-                properties: vertex.properties,
-              };
-              vertices.push(temp);
-            }
+      // If one of the objects is of type edges, we retrieve edges contained in it
+      if (element[0] === "edges" && element[1].elements) {
+        for (let edge of element[1].elements) {
+          //retrieve every properties of every arc that has properties different from null
+          if (edge.properties !== null && edge.properties !== undefined) {
+            let temp = {
+              id: edge.id,
+              ends: edge.ends,
+              properties: edge.properties,
+            };
+            edges.push(temp);
           }
         }
+      }
+
+      // If one of the objects is of type vertices, we retrieve vertices contained in it
+      if (element[0] === "vertices") {
+        for (let vertex of element[1].elements) {
+          //retrieve every properties of every arc that has properties different from null
+          if (vertex.properties !== null && vertex.properties !== undefined) {
+            let locations = vertex.properties.location.split(",");
+            let x = locations[0];
+            let y = locations[1];
+            let z = locations[2];
+
+            let temp = {
+              id: vertex.id,
+              x: x,
+              y: y,
+              z: z,
+              properties: vertex.properties,
+            };
+            vertices.push(temp);
+          }
+        }
+      }
     }
     entireObject = {
         arcs: arcs,
